a71926dac04048cc951fdf59c2a3048564470e98
Jip Veerbeek <97675309+JipVeerbeek@users.noreply.github.com>
2023-11-21 11:44:55 +0100

    map

--- src/App.js
+++ src/App.js
@@ -5,8 +5,17 @@ import HomeScreen from "./screens/home";
 import SettingsScreen from "./screens/settings";
 import VacationsScreen from "./screens/vacations";
 import AppHeader from "./components/header";
+import Icon from "react-native-vector-icons/FontAwesome";
 
 const Tab = createBottomTabNavigator();
+
+const TabIcon = ({ name, focused }) => (
+  <Icon
+    name={name}
+    size={focused ? 35 : 28}
+    color={focused ? "black" : "gray"}
+  />
+);
 
 export default function App() {
   return (
@@ -14,25 +23,41 @@ export default function App() {
       <Tab.Navigator
         initialRouteName="Welkom"
         screenOptions={{
-          tabBarStyle: { display: 'flex' },
-          tabBarItemStyle: { justifyContent: 'center' },
+          tabBarStyle: { display: "flex", height: 70 },
+          tabBarItemStyle: { justifyContent: "center" },
           header: (props) => <AppHeader {...props} />,
+          tabBarLabelStyle: { color: "black" },
         }}
       >
         <Tab.Screen
           name="Vacations"
           component={VacationsScreen}
-          options={{ tabBarLabel: "Vacations" }}
+          options={{
+            tabBarLabel: "Vacations",
+            tabBarIcon: ({ focused }) => (
+              <TabIcon name="suitcase" focused={focused} />
+            ),
+          }}
         />
         <Tab.Screen
           name="Welkom"
           component={HomeScreen}
-          options={{ tabBarLabel: "Home" }}
+          options={{
+            tabBarLabel: "Home",
+            tabBarIcon: ({ focused }) => (
+              <TabIcon name="home" focused={focused} />
+            ),
+          }}
         />
         <Tab.Screen
           name="Settings"
           component={SettingsScreen}
-          options={{ tabBarLabel: "Settings" }}
+          options={{
+            tabBarLabel: "Settings",
+            tabBarIcon: ({ focused }) => (
+              <TabIcon name="cog" focused={focused} />
+            ),
+          }}
         />
       </Tab.Navigator>
     </NavigationContainer>
--- src/screens/settings.js
+++ src/screens/settings.js
@@ -1,12 +1,77 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, StyleSheet, Text } from "react-native";
+import MapView, { Marker } from "react-native-maps";
+import * as Location from "expo-location";
 
-export default function SettingsScreen() {
+const SettingsScreen = () => {
+  const [location, setLocation] = useState(null);
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
+  const getLocation = async () => {
+    try {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== "granted") {
+        console.error("Permission to access location was denied");
+        return;
+      }
+
+      let currentLocation = await Location.getCurrentPositionAsync({});
+      setLocation(currentLocation);
+    } catch (error) {
+      console.error("Error getting location:", error);
+    }
+  };
+
   return (
-    <View>
-      <Text>settings Screen</Text>
-      {/* Add your screen content here */}
+    <View style={styles.container}>
+      {location ? (
+        location && (
+          <MapView
+            style={styles.map}
+            initialRegion={{
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+              latitudeDelta: 0.3,
+              longitudeDelta: 0.3,
+            }}
+            rotateEnabled={false}
+          >
+            <Marker
+              coordinate={{
+                latitude: location.coords.latitude,
+                longitude: location.coords.longitude,
+              }}
+              title="My Location"
+              description="This is my current location"
+            />
+          </MapView>
+        )
+      ) : (
+        <Text style={styles.error}>Fix je settings maat</Text>
+      )}
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    // justifyContent: "center",
+  },
+  map: {
+    // flex: 1,
+    width: "100%",
+    height: 300,
+  },
+  error: {
+    fontSize: 30,
+    fontWeight: "bold",
+    color: "red",
+  },
+});
+
+export default SettingsScreen;
